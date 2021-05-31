bad674cbc0a3a6ddd4ebfe1938e1f881f02856ca
mltsk <mltsk94@gmail.com>
2021-06-01 01:34:48 +0300

    refactoring

--- module/genDiff.js
+++ module/genDiff.js
@@ -10,12 +10,12 @@ const removePath = (property, item) => {
     return property;
 }
 
-export default function genDiff(data1, data2) {
+export default function genDiff(data1, data2, property = '') {
   
     const result = [];
 
-  const diff = (data1, data2, result, property = "") => {
-
+    const diff = (data1, data2, result) => {
+        
     const key1 = _.keys(data1);
     const key2 = _.keys(data2);
     const keys = _.uniq((key1).concat(key2));
@@ -24,70 +24,43 @@ export default function genDiff(data1, data2) {
 
     keysSorted.map((item) => {
         const temp = {};
-        
-        // result.push(`{ property: ${path(property, item)}`);
-        // temp.property = `${path(property, item)}`;
+        temp.name = `${item}`;
+        temp.property = `${path(property, `${item}`)}`;
 
         if((typeof(data1[item]) === 'object' && typeof(data2[item]) === 'object')) {
-            temp.name = `${item}`;
-            temp.property = `${path(property, `${item}`)}`;
             temp.status = "unchanged";
             temp.value = "[complex value]";
-            result.push(temp);
-            diff(data1[item], data2[item], result, property =`${path(property, `${item}`)}`);
-
+            temp.children = (genDiff(data1[item], data2[item], property =`${path(property, `${item}`)}`));
             property = removePath(property, `${item}`)
 
-        }
-        
-        else if (key1.includes(item) && key2.includes(item) && data1[item] === data2[item]) {
-            // result.push(` "status": "unchanged" "value": "${data1[item]} }"`);
-            temp.name = `${item}`;
-            temp.property = `${path(property, `${item}`)}`;
+        }else if (key1.includes(item) && key2.includes(item) && data1[item] === data2[item]) {
             temp.status = "unchanged" 
             temp.value = data1[item];
-            result.push(temp);
+
         } else if (key1.includes(item) && key2.includes(item) && data1[item] !== data2[item]) {
-            // result.push(` "status": "updated" "value": "${data1[item]}" "new value": "${data2[item]} }"`);
-            temp.name = `${item}`;
-            temp.property = `${path(property, `${item}`)}`;
             temp.status = "updated" 
             temp.value = data1[item]; 
             temp.newValue = data2[item];
-            result.push(temp);
+
         } else if (key1.includes(item)) {
-            // result.push(` "status": "removed" "value": "${data1[item]} }"`);
-            temp.name = `${item}`;
-            temp.property = `${path(property, `${item}`)}`;
             temp.status = "removed" 
             temp.value = data1[item]
-            result.push(temp); 
+
         } else {
-            // result.push(` "status": "added" "value": "${data2[item]} }"`);
-            temp.name = `${item}`;
-            temp.property = `${path(property, `${item}`)}`;
             temp.status = "added" 
             temp.value = data2[item]
-            // console.log('data2[item]: ', JSON.stringify(temp.value, null, 2));
-            // console.log('data2[item]: ', temp.value);
-            // console.log('data2[item]: ', console.log(Object.values(temp.value)));
-            result.push(temp); 
         }
         
-        
-        
-        
-        // return result;
+        result.push(temp); 
+
     });
-    // console.log('diff', result);
-    // result = _.sortBy(result, [function(o) { return o.property; }]);
-  }
+
+    return result;
+
+    }
   
-  diff(data1, data2, result);
-  // console.log('result', result.toString().replace(/[,]/g, ''));
-//   return (result.toString().replace(/[,]/g, ''));
-//   return (JSON.stringify(result));
-  return (result);
+    diff(data1, data2, result);
+    return result;
 }
 
-// console.log(genDiff('files/file1.json', 'files/file2.json'));
+
