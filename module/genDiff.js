86b359a057efe2d6c18984ccbec52ecc10dfb607
mltsk <mltsk94@gmail.com>
2021-06-09 19:31:44 +0300

    refactoring

--- module/genDiff.js
+++ module/genDiff.js
@@ -3,22 +3,21 @@ import { stylish, plain, json } from '../formatters/index.js';
 
 const path = (property1, property2) => (property1 ? `${property1}.${property2}` : property2);
 
-const removePath = (property, item) => {
-  property.replace(`.${item}`, '');
-  property.replace(`${item}`, '');
+const removePath = (property, key) => {
+  property.replace(`.${key}`, '');
+};
+
+const getFormatter = (format) => {
+  if (format === 'plain') {
+    return plain;
+  } if (format === 'json') {
+    return json;
+  }
+  return stylish;
 };
 
 export default function genDiff(file1, file2, format = 'stylish') {
-  const getFormatter = () => {
-    if (format === 'plain') {
-      return plain;
-    } if (format === 'json') {
-      return json;
-    }
-    return stylish;
-  };
-
-  function diff(data1, data2, property = '') {
+  const diff = (data1, data2, property = '') => {
     const result = [];
 
     const key1 = _.keys(data1);
@@ -26,37 +25,37 @@ export default function genDiff(file1, file2, format = 'stylish') {
     const keys = _.uniq((key1).concat(key2));
     const keysSorted = _.sortBy(keys);
 
-    keysSorted.forEach((item) => {
+    keysSorted.forEach((key) => {
       const temp = {};
-      temp.name = `${item}`;
-      temp.property = `${path(property, `${item}`)}`;
+      temp.name = `${key}`;
+      temp.property = `${path(property, `${key}`)}`;
 
-      if ((typeof (data1[item]) === 'object' && typeof (data2[item]) === 'object')) {
+      if ((typeof (data1[key]) === 'object' && typeof (data2[key]) === 'object')) {
         temp.status = 'unchanged';
         temp.value = '[complex value]';
-        temp.children = (diff(data1[item], data2[item], `${path(property, `${item}`)}`));
-        removePath(property, `${item}`);
-      } else if (key1.includes(item) && key2.includes(item) && data1[item] === data2[item]) {
+        temp.children = (diff(data1[key], data2[key], `${path(property, `${key}`)}`));
+        removePath(property, `${key}`);
+      } else if (key1.includes(key) && key2.includes(key) && data1[key] === data2[key]) {
         temp.status = 'unchanged';
-        temp.value = data1[item];
-      } else if (key1.includes(item) && key2.includes(item) && data1[item] !== data2[item]) {
+        temp.value = data1[key];
+      } else if (key1.includes(key) && key2.includes(key) && data1[key] !== data2[key]) {
         temp.status = 'updated';
-        temp.value = data1[item];
-        temp.newValue = data2[item];
-      } else if (key1.includes(item)) {
+        temp.value = data1[key];
+        temp.newValue = data2[key];
+      } else if (key1.includes(key)) {
         temp.status = 'removed';
-        temp.value = data1[item];
+        temp.value = data1[key];
       } else {
         temp.status = 'added';
-        temp.value = data2[item];
+        temp.value = data2[key];
       }
 
       result.push(temp);
     });
     return result;
-  }
+  };
 
-  const formatter = getFormatter();
+  const formatter = getFormatter(format);
 
   return formatter(diff(file1, file2));
 }
