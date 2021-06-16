137dfd9dc1d058f1fbbf11260c9120d3c818379f
Dmitrii Maletskii <mltsk94@gmail.com>
2021-06-16 18:10:58 +0300

    refactoring

--- module/stylish.js
+++ module/stylish.js
@@ -1,4 +1,4 @@
-const makeSpace = (number, symbol, correction = 0) => symbol.repeat(number + correction);
+const makeSpace = (offset, symbol, correction = 0) => symbol.repeat(offset + correction);
 
 const getPrefix = (status) => {
   const prefix = { added: '+ ', removed: '- ', unchanged: '  ' };
@@ -6,33 +6,36 @@ const getPrefix = (status) => {
 };
 
 const ObjectStylish = (obj, symbol, offset) => {
-  let result = '{\n';
+  const result = [];
+  result.push('{');
 
   Object.entries(obj).forEach((item) => {
     const key = item[0];
-    let tempValue = item[1];
-    if (typeof (tempValue) === 'object') {
-      tempValue = ObjectStylish(tempValue, symbol, offset + 4);
+    let value = item[1];
+    if (typeof (value) === 'object') {
+      value = ObjectStylish(value, symbol, offset + 4);
     }
-    result += `${makeSpace(offset, symbol)}${key}: ${tempValue}\n`;
+    result.push(`${makeSpace(offset, symbol)}${key}: ${value}`);
   });
-  result += `${makeSpace(offset - 4, symbol)}}`;
-  return result;
+  result.push(`${makeSpace(offset - 4, symbol)}}`);
+  return result.join('\n');
 };
 
 const stylish = (obj, symbol = ' ', offset = 4) => {
   const result = [];
   result.push('{');
   obj.forEach((item) => {
-    let { value } = item;
+    let { value, newValue } = item;
     if (typeof (item.children) === 'object') {
       value = stylish(item.children, symbol, offset + 4);
     } else if (typeof (value) === 'object') {
       value = ObjectStylish(value, symbol, offset + 4);
+    } else if (typeof (newValue) === 'object' && newValue !== null) {
+      newValue = ObjectStylish(newValue, symbol, offset + 4);
     }
     if (item.status === 'updated') {
       result.push(`${makeSpace(offset, symbol, -2)}${getPrefix('removed')}${item.name}: ${value}`);
-      result.push(`${makeSpace(offset, symbol, -2)}${getPrefix('added')}${item.name}: ${item.newValue}`);
+      result.push(`${makeSpace(offset, symbol, -2)}${getPrefix('added')}${item.name}: ${newValue}`);
     } else {
       result.push(`${makeSpace(offset, symbol, -2)}${getPrefix(item.status)}${item.name}: ${value}`);
     }
