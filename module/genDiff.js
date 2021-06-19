1e8e978f9e467db76b8dfdd5b6dacd718a671ac0
Dmitrii Maletskii <mltsk94@gmail.com>
2021-06-20 00:53:47 +0300

    refactoring

--- module/genDiff.js
+++ module/genDiff.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { stylish, plain, json } from '../formatters/index.js';
 import parse from './parsers.js';
 
-const path = (property1, property2) => (property1 ? `${property1}.${property2}` : property2);
+const makePath = (property1, property2) => (property1 ? `${property1}.${property2}` : property2);
 
 const removePath = (property, key) => {
   property.replace(`.${key}`, '');
@@ -31,12 +31,12 @@ export default function genDiff(filepath1, filepath2, format) {
     keysSorted.forEach((key) => {
       const temp = {};
       temp.name = `${key}`;
-      temp.property = `${path(property, `${key}`)}`;
+      temp.property = `${makePath(property, `${key}`)}`;
 
       if ((typeof (data1[key]) === 'object' && typeof (data2[key]) === 'object')) {
         temp.status = 'unchanged';
         temp.value = '[complex value]';
-        temp.children = (diff(data1[key], data2[key], `${path(property, `${key}`)}`));
+        temp.children = (diff(data1[key], data2[key], `${makePath(property, `${key}`)}`));
         removePath(property, `${key}`);
       } else if (key1.includes(key) && key2.includes(key) && data1[key] === data2[key]) {
         temp.status = 'unchanged';
