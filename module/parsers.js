a3080ba21a649905e9b53e3bae21f06e2b6bb61a
Dmitrii Maletskii <mltsk94@gmail.com>
2021-06-30 22:38:40 +0300

    refactoring

--- module/parsers.js
+++ module/parsers.js
@@ -1,24 +1,19 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const getFixturePath = (filename) => {
-  if (path.isAbsolute(filename)) {
-    return filename;
-  }
-  return path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (fileName) => {
+  const fullPath = path.resolve(process.cwd(), '__fixtures__', fileName);
+  const data = fs.readFileSync(fullPath, 'utf8').toString();
+  return data;
 };
 
-const parse = (filename) => {
-  const expansion = path.extname(filename);
+const parse = (fileName) => {
+  const expansion = path.extname(fileName);
   if (expansion === '.yml' || expansion === '.yaml') {
-    return yaml.load(fs.readFileSync(getFixturePath(filename), 'utf8'));
+    return yaml.load(readFile(fileName));
   }
-  return JSON.parse(fs.readFileSync(getFixturePath(filename), 'utf8'));
+  return JSON.parse(readFile(fileName));
 };
 
 export default parse;
