cb95f5fab6848076c1737e4ee09a386c7a847ff7
Dmitrii Maletskii <mltsk94@gmail.com>
2021-07-01 16:28:19 +0300

    refactoring diff()

--- module/genDiff.js
+++ module/genDiff.js
@@ -22,43 +22,31 @@ export default function genDiff(filepath1, filepath2, format) {
   const fileData2 = parse(filepath2);
 
   const diff = (data1, data2, property) => {
-    const result = [];
     const key1 = _.keys(data1);
     const key2 = _.keys(data2);
     const keys = _.uniq((key1).concat(key2));
     const keysSorted = _.sortBy(keys);
 
-    keysSorted.forEach((key) => {
-      const temp = {};
-      temp.name = `${key}`;
-      temp.property = `${makePath(property, `${key}`)}`;
-
+    const result = keysSorted.reduce((acc, key) => {
+      const nameAndProperty = { name: `${key}`, property: `${makePath(property, `${key}`)}` };
+      removePath(property, `${key}`);
       if ((typeof (data1[key]) === 'object' && typeof (data2[key]) === 'object')) {
-        temp.status = 'unchanged';
-        temp.value = '[complex value]';
-        temp.children = (diff(data1[key], data2[key], `${makePath(property, `${key}`)}`));
-        removePath(property, `${key}`);
-      } else if (key1.includes(key) && key2.includes(key) && data1[key] === data2[key]) {
-        temp.status = 'unchanged';
-        temp.value = data1[key];
-      } else if (key1.includes(key) && key2.includes(key) && data1[key] !== data2[key]) {
-        temp.status = 'updated';
-        temp.value = data1[key];
-        temp.newValue = data2[key];
-      } else if (key1.includes(key)) {
-        temp.status = 'removed';
-        temp.value = data1[key];
-      } else {
-        temp.status = 'added';
-        temp.value = data2[key];
+        return [...acc, {
+          ...nameAndProperty, status: 'unchanged', value: '[complex value]', children: diff(data1[key], data2[key], makePath(property, key)),
+        }];
+      } if (key1.includes(key) && key2.includes(key) && data1[key] === data2[key]) {
+        return [...acc, { ...nameAndProperty, status: 'unchanged', value: data1[key] }];
+      } if (key1.includes(key) && key2.includes(key) && data1[key] !== data2[key]) {
+        return [...acc, {
+          ...nameAndProperty, status: 'updated', value: data1[key], newValue: data2[key],
+        }];
+      } if (key1.includes(key)) {
+        return [...acc, { ...nameAndProperty, status: 'removed', value: data1[key] }];
       }
-
-      result.push(temp);
-    });
+      return [...acc, { ...nameAndProperty, status: 'added', value: data2[key] }];
+    }, '');
     return result;
   };
-
   const formatter = getFormatter(format);
-
   return formatter(diff(fileData1, fileData2, ''));
 }
