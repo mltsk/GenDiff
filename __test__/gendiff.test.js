7c8c5d69199a9837017e2d44ad6d0726d8081f90
Dmitrii Maletskii <mltsk94@gmail.com>
2021-08-01 15:25:54 +0300

    refactoring

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -9,31 +9,28 @@ const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__
 
 const jsonFilePath1 = getFixturePath('file1.json');
 const jsonFilePath2 = getFixturePath('file2.json');
+const yamlFilePath1 = getFixturePath('file1.yaml');
+const yamlFilePath2 = getFixturePath('file2.yaml');
+const expectedStylish = fs.readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
+const expectedPlain = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
+const expectedJson = fs.readFileSync(getFixturePath('expected_json.txt'), 'utf-8');
 
 test('stylish json', () => {
-  const expected = fs.readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
   const actual = genDiff(jsonFilePath1, jsonFilePath2, 'stylish');
-  expect(actual).toBe(expected);
+  expect(actual).toBe(expectedStylish);
 });
 
 test('stylish yaml', () => {
-  const expected = fs.readFileSync(getFixturePath('expected_stylish.txt'), 'utf-8');
-  const yamlFilePath1 = getFixturePath('file1.yaml');
-  const yamlFilePath2 = getFixturePath('file2.yaml');
   const actual = genDiff(yamlFilePath1, yamlFilePath2, 'stylish');
-  expect(actual).toBe(expected);
+  expect(actual).toBe(expectedStylish);
 });
 
 test('plain', () => {
-  const expected = fs.readFileSync(getFixturePath('expected_plain.txt'), 'utf-8');
-
   const actual = genDiff(jsonFilePath1, jsonFilePath2, 'plain');
-  expect(actual).toEqual(expected);
+  expect(actual).toEqual(expectedPlain);
 });
 
 test('json', () => {
-  const expected = fs.readFileSync(getFixturePath('expected_json.txt'), 'utf-8');
-
   const actual = genDiff(jsonFilePath1, jsonFilePath2, 'json');
-  expect(actual).toEqual(expected);
+  expect(actual).toEqual(expectedJson);
 });
