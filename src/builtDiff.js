0fcbdefa6840fe3a9854a99cd9e11a7071401d79
Dmitrii Maletskii <mltsk94@gmail.com>
2021-08-08 19:13:58 +0300

    refactoring

---
+++ src/builtDiff.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+
+const builtDiff = (data1, data2) => {
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
+  const sortedKeys = _.sortBy(_.union(keys2, keys1));
+
+  const result = sortedKeys.map((key) => {
+    if (!_.has(data2, key)) {
+      return {
+        name: key, type: 'removed', value: data1[key],
+      };
+    } if (!_.has(data1, key)) {
+      return {
+        name: key, type: 'added', value: data2[key],
+      };
+    } if ((_.isObject(data1[key]) && _.isObject(data2[key]))) {
+      return {
+        name: key, type: 'unchanged', value: '[complex value]', children: builtDiff(data1[key], data2[key]),
+      };
+    } if (data1[key] === data2[key]) {
+      return {
+        name: key, type: 'unchanged', value: data1[key],
+      };
+    }
+    return {
+      name: key, type: 'updated', value: data1[key], newValue: data2[key],
+    };
+  });
+  return result;
+};
+
+export default builtDiff;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,13 @@
-import _ from 'lodash';
 import getFormatter from './formatters/index.js';
 import parse from './parsers.js';
 import getFileData from './getFileData.js';
+import builtDiff from './builtDiff.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileData1 = parse(...getFileData(filepath1));
   const fileData2 = parse(...getFileData(filepath2));
-
-  const diff = (data1, data2) => {
-    const keys1 = _.keys(data1);
-    const keys2 = _.keys(data2);
-    const sortedKeys = _.sortBy(_.union(keys2, keys1));
-
-    const result = sortedKeys.map((key) => {
-      if (!_.has(data2, key)) {
-        return {
-          name: key, type: 'removed', value: data1[key],
-        };
-      } if (!_.has(data1, key)) {
-        return {
-          name: key, type: 'added', value: data2[key],
-        };
-      } if ((_.isObject(data1[key]) && _.isObject(data2[key]))) {
-        return {
-          name: key, type: 'unchanged', value: '[complex value]', children: diff(data1[key], data2[key]),
-        };
-      } if (data1[key] === data2[key]) {
-        return {
-          name: key, type: 'unchanged', value: data1[key],
-        };
-      }
-      return {
-        name: key, type: 'updated', value: data1[key], newValue: data2[key],
-      };
-    });
-    return result;
-  };
   const formatter = getFormatter(formatName);
-  return formatter(diff(fileData1, fileData2));
+  return formatter(builtDiff(fileData1, fileData2));
 };
 
 export default genDiff;
